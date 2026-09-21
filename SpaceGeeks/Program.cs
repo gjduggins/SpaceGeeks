c3c9e3775bfa9b9478ddcbd06b138aee397d5373
gjduggins <55781433+gjduggins@users.noreply.github.com>
2026-09-21 13:12:07 +0100

    Register INasaMissionRepository service

--- SpaceGeeks/Program.cs
+++ SpaceGeeks/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<SpaceGeeks.Data.IPlanetRepository, SpaceGeeks.Data.InMemoryPlanetRepository>();
+builder.Services.AddSingleton<SpaceGeeks.Data.INasaMissionRepository, SpaceGeeks.Data.InMemoryNasaMissionRepository>();
 
 var app = builder.Build();
 
