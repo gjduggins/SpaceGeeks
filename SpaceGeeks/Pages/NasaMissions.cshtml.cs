017a8819f65ac796834d1ed94630efe672353e2b
gjduggins <55781433+gjduggins@users.noreply.github.com>
2026-09-21 13:39:08 +0100

    Add NASA missions page model

---
+++ SpaceGeeks/Pages/NasaMissions.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpaceGeeks.Pages;
+
+public class NasaMissionsModel : PageModel
+{
+    public void OnGet()
+    {
+        // This page doesn't require any special data loading
+    }
+}
